87532a1a18e2f09f3260c7aeb45823dc269d1578
从今以后 <949478479@qq.com>
2017-03-03 04:47:42 +0800

    上传GitHub

---
+++ LXNavigationBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXNavigationBar
+//
+//  Created by 从今以后 on 17/3/3.
+//  Copyright © 2017年 从今以后. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
